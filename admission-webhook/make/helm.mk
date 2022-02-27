89420fcceafe0fb8a9baef1e28d41d9945d24033
Jamie Phillips <cphill11@gmail.com>
2022-02-27 15:24:35 -0500

    Just plumbing.
    Signed-off-by: Jamie Phillips <cphill11@gmail.com>

--- admission-webhook/make/helm.mk
+++ admission-webhook/make/helm.mk
@@ -13,3 +13,38 @@ helm-index:
 .PHONY: helm-lint
 helm-lint:
 	helm lint ../charts/$(VERSION)/gmsa
+
+# deploys the chart to the kind cluster with the release image
+.PHONY: deploy_chart
+deploy_chart:
+	K8S_GMSA_IMAGE=$(IMAGE_NAME) $(MAKE) _deploy_webhook
+
+# removes the chart from the kind cluster
+.PHONY: remove_chart
+remove_chart	:
+ifeq ($(wildcard $(MANIFESTS_FILE)),)
+	@ echo "No manifests file found at $(MANIFESTS_FILE), nothing to remove"
+else
+	KUBECONFIG=$(KUBECONFIG) $(KUBECTL) delete -f $(MANIFESTS_FILE) || true
+endif
+
+# deploys the webhook to the kind cluster using helm
+# if $K8S_GMSA_DEPLOY_METHOD is set to "download", then it will deploy by downloading
+# the deploy script as documented in the README, using $K8S_GMSA_DEPLOY_CHART_REPO and
+# $K8S_GMSA_DEPLOY_CHART_VERSION env variables to build the download URL. If VERSION is
+# not set then latest is used.
+.PHONY: _deploy_chart
+_deploy_chart: remove_chart
+ifeq ($(K8S_GMSA_CHART),)
+	@ echo "Cannot call target $@ without setting K8S_GMSA_CHART"
+	exit 1
+endif
+	mkdir -p $(dir $(MANIFESTS_FILE))
+ifeq ($(K8S_GMSA_DEPLOY_METHOD),download)
+	@if [ ! "$$K8S_GMSA_DEPLOY_DOWNLOAD_REPO" ]; then K8S_GMSA_DEPLOY_DOWNLOAD_REPO="kubernetes-sigs/windows-gmsa"; fi \
+      && if [ ! "$$K8S_GMSA_DEPLOY_DOWNLOAD_REV" ]; then K8S_GMSA_DEPLOY_DOWNLOAD_REV="$$(git rev-parse HEAD)"; fi \
+      && CMD="curl -sL 'https://raw.githubusercontent.com/$$K8S_GMSA_DEPLOY_DOWNLOAD_REPO/$$K8S_GMSA_DEPLOY_DOWNLOAD_REV/admission-webhook/deploy/deploy-gmsa-webhook.sh' | K8S_GMSA_DEPLOY_DOWNLOAD_REPO='$$K8S_GMSA_DEPLOY_DOWNLOAD_REPO' K8S_GMSA_DEPLOY_DOWNLOAD_REV='$$K8S_GMSA_DEPLOY_DOWNLOAD_REV' KUBECONFIG=$(KUBECONFIG) KUBECTL=$(KUBECTL) bash -s -- --file '$(MANIFESTS_FILE)' --name '$(DEPLOYMENT_NAME)' --namespace '$(NAMESPACE)' --image '$(K8S_GMSA_IMAGE)' --certs-dir '$(CERTS_DIR)' $(EXTRA_GMSA_DEPLOY_ARGS)" \
+      && echo "$$CMD" && eval "$$CMD"
+else
+	KUBECONFIG=$(KUBECONFIG) KUBECTL=$(KUBECTL) ./deploy/deploy-gmsa-webhook.sh --file "$(MANIFESTS_FILE)" --name "$(DEPLOYMENT_NAME)" --namespace "$(NAMESPACE)" --image "$(K8S_GMSA_IMAGE)" --certs-dir "$(CERTS_DIR)" $(EXTRA_GMSA_DEPLOY_ARGS)
+endif
